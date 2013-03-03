ac6ca553302ce966dcfed6c1d3f1ed1c335961b5
Shun Zhang <cs.utexas.edu>
2013-03-03 16:13:04 -0600

    part 1 done: stack works

--- src/userprog/build/lib/user/syscall.d
+++ src/userprog/build/lib/user/syscall.d
@@ -1,2 +1,3 @@
 lib/user/syscall.o: ../../lib/user/syscall.c ../../lib/user/syscall.h \
-  ../../lib/stdbool.h ../../lib/debug.h ../../lib/user/../syscall-nr.h
+  ../../lib/stdbool.h ../../lib/debug.h ../../lib/user/../syscall-nr.h \
+  ../../lib/user/../kernel/console.h ../../lib/user/../kernel/../stddef.h
