7a6e38f4cceafa3c89680d1be9e3b99887bdc68f
Shun Zhang <menie482@cs.utexas.edu>
2013-03-22 18:33:58 -0500

    read me ready

--- src/userprog/build/lib/user/syscall.d
+++
@@ -1,3 +0,0 @@
-lib/user/syscall.o: ../../lib/user/syscall.c ../../lib/user/syscall.h \
-  ../../lib/stdbool.h ../../lib/debug.h ../../lib/user/../syscall-nr.h \
-  ../../lib/user/../kernel/console.h ../../lib/user/../kernel/../stddef.h
--- src/userprog/build/userprog/syscall.d
+++
@@ -1,8 +0,0 @@
-userprog/syscall.o: ../../userprog/syscall.c ../../userprog/syscall.h \
-  ../../lib/stdio.h ../../lib/debug.h ../../lib/stdarg.h \
-  ../../lib/stdbool.h ../../lib/stddef.h ../../lib/stdint.h \
-  ../../lib/kernel/stdio.h ../../lib/syscall-nr.h \
-  ../../threads/interrupt.h ../../threads/thread.h \
-  ../../lib/kernel/list.h ../../threads/synch.h ../../threads/vaddr.h \
-  ../../threads/loader.h ../../userprog/pagedir.h \
-  ../../userprog/process.h ../../lib/string.h ../../devices/shutdown.h
